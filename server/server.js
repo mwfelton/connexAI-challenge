b11c2dcc44e5557a71db8eaa0ee3b0b101f1b485
Miles Felton Gerber <mwfelton03@gmail.com>
2024-11-29 11:34:32 +0000

    Auth header added

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+app.use((req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  if (authHeader !== 'mysecrettoken') {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+  next();
+});
+
 app.get('/time', (req, res) => {
   const serverTime = {
     epoch: Math.floor(Date.now() / 1000),
